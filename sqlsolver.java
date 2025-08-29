cef69c7f114f9996f0a8aac0aa208fdad125a387
Kushal k <142097319+kushalk0410@users.noreply.github.com>
2025-08-29 10:40:41 +0530

    Create sqlsolver.java

---
+++ sqlsolver.java
@@ -0,0 +1,12 @@
+package com.example.sqlsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// This starts the Spring Boot app
+@SpringBootApplication
+public class SqlSolverApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SqlSolverApp.class, args);
+    }
+}
